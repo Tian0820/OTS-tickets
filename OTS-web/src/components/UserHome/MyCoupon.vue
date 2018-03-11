299479d9e6c9ecab4a1d3d58cd181b752d141bdd
Tian0820 <151250121@smail.nju.edu.cn>
2018-03-11 13:30:53 +0800

    <dev>:<add user coupon blank part>

---
+++ OTS-web/src/components/UserHome/MyCoupon.vue
@@ -0,0 +1,29 @@
+<template>
+
+  <div class="my-coupon-wrapper">
+    <div-header :header="'我的优惠券'"></div-header>
+
+
+  </div>
+
+
+</template>
+
+<script>
+  import DivHeader from '../Util/DivHeader.vue'
+  import {router} from '../../main'
+
+  export default {
+    name: 'my-coupon',
+    components: {
+      DivHeader
+    },
+    data() {
+      return {}
+    },
+    methods: {}
+  }
+
+</script>
+
+<style scoped src="./MyCoupon.css"></style>
--- OTS-web/src/components/UserHome/UserHome.vue
+++ OTS-web/src/components/UserHome/UserHome.vue
@@ -3,6 +3,7 @@
   <div class="user-home-wrapper">
     <user-info-brief></user-info-brief>
     <my-order></my-order>
+    <my-coupon></my-coupon>
   </div>
 
 
@@ -12,12 +13,14 @@
   import {router} from '../../main'
   import UserInfoBrief from '../Admin/UserInfoBrief.vue'
   import MyOrder from '../UserHome/MyOrder.vue'
+  import MyCoupon from '../UserHome/MyCoupon.vue'
 
   export default {
     name: 'user-home',
     components: {
       UserInfoBrief,
-      MyOrder
+      MyOrder,
+      MyCoupon
     },
     data() {
       return {}
