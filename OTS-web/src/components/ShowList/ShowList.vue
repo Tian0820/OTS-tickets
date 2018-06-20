088d44ea6085ce551814c45e91b7ac82f8b8060a
Sorumi <Sorumi33@gmail.com>
2018-06-20 10:29:06 +0800

    <dev>:<update show list style>

--- OTS-web/src/components/ShowList/ShowList.vue
+++ OTS-web/src/components/ShowList/ShowList.vue
@@ -2,19 +2,30 @@
 
   <div class="list-wrapper">
     <div-header :header="'最近演出'"></div-header>
+
     <el-row :gutter="40">
-      <single-show v-for="item in showPlans" :key="item.id" :show="item"></single-show>
+        <el-col :xs="12" :sm="8" :md="6" :lg="6" v-for="item in showPlans"  :key="item.id">
+          <single-show :show="item"></single-show>
+        </el-col>
     </el-row>
 
-  </div>
+    <div class="pagination">
+      <el-pagination
+        background
+        layout="prev, pager, next"
+        :total="1000">
+      </el-pagination>
+    </div>
 
+
+  </div>
 
 </template>
 
 <script>
   import DivHeader from '../Util/DivHeader.vue'
   import SingleShow from './SingleShow.vue'
-  import {Row} from 'element-ui'
+  import {Row, Col, Pagination} from 'element-ui'
   import {store} from '../../main'
   import {mapActions} from 'vuex'
 
@@ -22,14 +33,19 @@
     name: 'show-list',
     components: {
       elRow: Row,
+      elCol: Col,
+      elPagination: Pagination,
       DivHeader,
       SingleShow
     },
     props: ['showPlans'],
     data() {
-      return {}
+        return {}
     },
-    methods: {},
+    computed: {
+    },
+    methods: {
+    },
   }
 </script>
 
