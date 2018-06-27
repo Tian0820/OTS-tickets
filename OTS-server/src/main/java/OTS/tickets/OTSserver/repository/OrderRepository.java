f41a054e2ed3d2a4eb62fe30121937ac9c038482
Cflelf <664158315@qq.com>
2018-06-27 11:27:02 +0800

    <fix>:<get order>

--- OTS-server/src/main/java/OTS/tickets/OTSserver/repository/OrderRepository.java
+++ OTS-server/src/main/java/OTS/tickets/OTSserver/repository/OrderRepository.java
@@ -2,12 +2,13 @@ package OTS.tickets.OTSserver.repository;
 
 import OTS.tickets.OTSserver.model.Order;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
-public interface OrderRepository extends JpaRepository<Order, Integer> {
+public interface OrderRepository extends JpaRepository<Order, Integer>, JpaSpecificationExecutor<Order> {
 
     Order findOrderById(int id);
 
