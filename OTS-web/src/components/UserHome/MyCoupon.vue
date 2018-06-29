d570ce5e081b895ff7201d7517c5ba02c451ea79
Tian0820 <151250121@smail.nju.edu.cn>
2018-06-29 11:44:13 +0800

    <dev>:<optimize user coupon part>

--- OTS-web/src/components/UserHome/MyCoupon.vue
+++ OTS-web/src/components/UserHome/MyCoupon.vue
@@ -2,7 +2,11 @@
 
   <div class="my-coupon-wrapper">
     <div-header :header="'我的优惠券'"></div-header>
-    <single-coupon v-for="item in coupons" :coupon="item"></single-coupon>
+    <el-button type="primary" @click="handleExchangeCoupon" :disabled="user.point.toFixed(2) === '0.00'">兑换优惠券
+    </el-button>
+    <div class="coupons-wrapper">
+      <single-coupon v-for="item in coupons" :coupon="item"></single-coupon>
+    </div>
 
   </div>
 
@@ -12,24 +16,35 @@
 <script>
   import DivHeader from '../Util/DivHeader.vue'
   import SingleCoupon from '../Coupon/SingleCoupon.vue'
+  import {Tooltip, Button} from 'element-ui'
   import {router} from '../../main'
-  import {mapState} from 'vuex'
+  import {mapState, mapMutations} from 'vuex'
 
   export default {
     name: 'my-coupon',
     components: {
       DivHeader,
-      SingleCoupon
+      SingleCoupon,
+      elTooltip: Tooltip,
+      elButton: Button
     },
     computed: {
       ...mapState('auth', {
-        coupons: state => state.userCoupons
+        coupons: state => state.userCoupons,
+        user: state => state.currentUser
       })
     },
     data() {
       return {}
     },
-    methods: {}
+    methods: {
+      ...mapMutations('auth', [
+        'saveExchangeCouponModal'
+      ]),
+      handleExchangeCoupon() {
+        this.saveExchangeCouponModal(true)
+      }
+    }
   }
 
 </script>
